575411298733771f5ca882f91d31002fd8196bf1
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-10-28 21:35:49 -0400

    add new-question component, add new-question template and js, modify index.hbs to display new-question button

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,12 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-  actions: {
+  actions:  {
+    submitQuestion3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
