f3cbe33b53b9fbbed75f25e094170e62ad40b2c7
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-10-30 19:06:24 -0400

    put delete function into edit-question template and update components, make minor changes to button margins

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -14,6 +14,11 @@ export default Ember.Component.extend({
       };
       this.set('editQuestionForm', false);
       this.sendAction('edit', question, params);
+    },
+    delete(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('destroyQuestion', question);
+      }
     }
   }
 });
