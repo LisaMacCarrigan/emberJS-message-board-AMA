a7cba683aacf4d87dae85570e881376dbbbb962a
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-04 18:10:51 -0400

    modify new-answer hbs template reference to render answers correctly

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
       if(this.get('answer')) {
         var params = {
           answer: this.get('answer'),
-          author: this.get('author'),
+          author: this.get('author_answer'),
         };
         if (params.author === '') {
           params.author = "Anonymous";
