5e6b76deb6aaec0dad20db5718a8f8168d81aefd
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-12 17:40:03 -0500

    improve home page question list styles, add styles to helper, begin to improve answer list styles

--- app/helpers/question-answered.js
+++ app/helpers/question-answered.js
@@ -4,7 +4,7 @@ export function questionAnswered(params/*, hash*/) {
   var question = params[0];
 
   if(question.get('answers').get('length') >=1) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-ok">Answered</span>');
+    return Ember.String.htmlSafe('<div id="checkmark"><span class="glyphicon glyphicon-ok"></span><span> Answered</span></div>');
   }
 }
 
