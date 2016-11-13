90fb1451e78739411b98282c71cab4c96ce6f930
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2016-11-13 10:04:30 -0500

    work on upvote feature

--- app/components/answer-list.js
+++ app/components/answer-list.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    upvote() {
+      var votes = this.get('votes');
+      votes ++;
+      this.set('votes', votes);
+      this.save();
+    }
+  }
 });
